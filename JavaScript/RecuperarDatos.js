c09f52e3a1d32a67712567569de9d7b702e6a026
Tarea1ProWeb <tarea1proweb@gmail.com>
2021-05-24 12:48:34 -0500

    Cabio en Index y JavaScript

--- JavaScript/RecuperarDatos.js
+++ JavaScript/RecuperarDatos.js
@@ -1,31 +1,11 @@
 var forma = document.getElementById("forma"),
-    salidaNombre = document.getElementById("salidaNombre"),
-    salidaSecreto = document.getElementById("salidaSecreto"),
-    salidaNavegador = document.getElementById("salidaNavegador"),
-    salidaEmail = document.getElementById("salidaEmail"),
-    salidaUsuario = document.getElementById("salidaUsuario"),
-    salidaUrl = document.getElementById("salidaUrl"),
-    salidaTelefono = document.getElementById("salidaTelefono"),
-    salidaVerso = document.getElementById("salidaVerso");
-
+    salidaNombre = document.getElementById("salidaNombre");
+   
 forma.addEventListener("submit", recuperar, false);
 
 function recuperar(){
     var nombre = forma["nombre"].value;
     salidaNombre.textContent = nombre;
-    var secreto = forma["secreto"].value;
-    salidaSecreto.textContent = secreto;
-    var navegador = forma["navegador"].value;
-    salidaNavegador.textContent = navegador;
-    var email = forma["email"].value;
-    salidaEmail.textContent = email;
-    var usuario = forma["usuario"].value;
-    salidaUsuario.textContent = usuario;
-    var url = forma["url"].value;
-    salidaUrl.textContent = url;
-    var telefono = forma["telefono"].value;
-    salidaTelefono.textContent = telefono;
-    var verso = forma["verso"].value;
-    salidaVerso.textContent = verso;
+    
 
 }
