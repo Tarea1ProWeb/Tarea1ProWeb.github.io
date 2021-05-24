34b40c7b2ced1581a741e903fcde0d54727f1625
Tarea1ProWeb <tarea1proweb@gmail.com>
2021-05-24 13:53:23 -0500

    Materia JS

--- JavaScript/RecuperarDatos.js
+++ JavaScript/RecuperarDatos.js
@@ -1,17 +1,20 @@
 var forma = document.getElementById("forma"),
     salidaBoleta = document.getElementById("salidaBoleta"),
     salidaNombre = document.getElementById("salidaNombre"),
-    salidaSecuencia = document.getElementById("salidaSecuencia");
+    salidaSecuencia = document.getElementById("salidaSecuencia"),
+    salidaMateria = document.getElementById("salidaMateria");
    
 forma.addEventListener("submit", recuperar, false);
 
 function recuperar(){
     var boleta = forma["boleta"].value,
         nombre = forma["nombre"].value,
-        secuencia = forma["secuencia"].value;
+        secuencia = forma["secuencia"].value,
+        materia = forma["materia"].value;
     salidaBoleta.textContent = boleta;
     salidaNombre.textContent = nombre;
     salidaSecuencia.textContent = secuencia;
+    materia.textContent = materia;
     
 
 }
