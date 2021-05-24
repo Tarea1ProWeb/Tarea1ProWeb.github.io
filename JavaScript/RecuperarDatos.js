1106bc3f85737cc4d7e80aff921bf431f227b206
Tarea1ProWeb <tarea1proweb@gmail.com>
2021-05-24 14:08:46 -0500

    HTML Y JS Fecha

--- JavaScript/RecuperarDatos.js
+++ JavaScript/RecuperarDatos.js
@@ -1,8 +1,10 @@
 var forma = document.getElementById("forma"),
+    txtFecha = forma["fecha"],
     salidaBoleta = document.getElementById("salidaBoleta"),
     salidaNombre = document.getElementById("salidaNombre"),
     salidaSecuencia = document.getElementById("salidaSecuencia"),
-    salidaMateria = document.getElementById("salidaMateria");
+    salidaMateria = document.getElementById("salidaMateria"),
+    salidaFecha = document.getElementById("salidaFecha");
    
 forma.addEventListener("submit", recuperar, false);
 
@@ -10,11 +12,13 @@ function recuperar(){
     var boleta = forma["boleta"].value,
         nombre = forma["nombre"].value,
         secuencia = forma["secuencia"].value,
-        materia = forma["materia"].value;
+        materia = forma["materia"].value,
+        fecha = new Date(txtFecha.value);
+        salidaFecha.textContent = (fecha.getDate()+1)+"/0"+(fecha.getMonth()+1)+"/"+(fecha.getFullYear());
     salidaBoleta.textContent = boleta;
     salidaNombre.textContent = nombre;
     salidaSecuencia.textContent = secuencia;
-    materia.textContent = materia;
+    salidaMateria.textContent = materia;
     
 
 }
