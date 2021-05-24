c0224d2d8750b84672db42437ea5509306c32b4d
Tarea1ProWeb <tarea1proweb@gmail.com>
2021-05-24 13:48:00 -0500

    Secuencia JS

--- JavaScript/RecuperarDatos.js
+++ JavaScript/RecuperarDatos.js
@@ -1,14 +1,17 @@
 var forma = document.getElementById("forma"),
     salidaBoleta = document.getElementById("salidaBoleta"),
-    salidaNombre = document.getElementById("salidaNombre");
+    salidaNombre = document.getElementById("salidaNombre"),
+    salidaSecuencia = document.getElementById("salidaSecuencia");
    
 forma.addEventListener("submit", recuperar, false);
 
 function recuperar(){
     var boleta = forma["boleta"].value,
-    nombre = forma["nombre"].value;
+        nombre = forma["nombre"].value,
+        secuencia = forma["secuencia"].value;
     salidaBoleta.textContent = boleta;
     salidaNombre.textContent = nombre;
+    salidaSecuencia.textContent = secuencia;
     
 
 }
